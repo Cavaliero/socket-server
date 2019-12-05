c0d5d52b1511421667e312a24dd865668d443c99
XE72168 <francisco.carrero.contractor@bbva.com>
2019-12-05 10:13:01 +0100

    seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,52 @@
 
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+//Configuración de la conexión de los sockets
+import socketIO from 'socket.io';
+import http from 'http';
+
+import * as sock from '../sockets/sockets';
 
 export default class Server {
+    private static _instance: Server;
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    // Propiedad encargada de eventos de los sockets
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server( this.app );
+        // Servidor de sockets
+        // No nos vale con el de express, tiene que ser el http de node
+        this.io = socketIO( this.httpServer );
+
+        this.listenSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+
+    private listenSockets() {
+
+        console.log('Escuchando conexiones...');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+            // Mensajes
+            sock.mensaje(cliente, this.io);
+
+            // Desconectar
+            sock.desconectar(cliente);
+        });
     }
 
     start( callback: Function) {
-        this.app.listen(this.port, callback);
+        //this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
